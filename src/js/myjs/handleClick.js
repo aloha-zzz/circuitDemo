e10cb57fd86950c1b6180a4ae3e54fc8571e34d4
zhou <835390899@qq.com>
2017-11-26 17:43:03 +0800

    demo

--- src/js/myjs/handleClick.js
+++ src/js/myjs/handleClick.js
@@ -30,26 +30,52 @@ document.getElementById('newSearch').onclick=function () {
     input[3].value=inputValue2;
     input[4].value='';
 }
-
+var bool1;
+var bool2;
 document.getElementById('compare').onclick=function () {
-    document.querySelectorAll('.btn.btn-primary.md-menu-white')[0].click()
-    document.getElementById('tree').style.display='none';
-    document.getElementById('main').style.display='block';
-    document.getElementById('main2').style.display='block';
-    createMap(mapdata);
-    createMapchart();
-    
+    var input=document.querySelectorAll('input.form-control');
+    if(input[2].value==''||input[4].value==''){
+        alert('click tree');
+    }else {
+        processData(input[1].value,input[2].value,input[3].value,input[4].value,bool1,bool2);
+        document.querySelectorAll('.btn.btn-primary.md-menu-white')[0].click()
+        document.getElementById('tree').style.display='none';
+        document.getElementById('main').style.display='block';
+        document.getElementById('main2').style.display='block';
+        createMap(mapdata);
+        createMapchart();
+    }
+
+
+
+
 
 }
 function treeClick(param) {
     var input=document.querySelectorAll('input.form-control');
-    console.log(param);
-    if(param.seriesName=='PMS'){
 
+    console.log(param);
+    if(param.data.name=='青阳变电站'){
+        return;
+    }
+
+    if(param.seriesName=='PMS'){
         input[2].value=param.data.name;
+        if(param.data.children==undefined){
+            bool1=false;
+        }else {
+            bool1=true;
+        }
     }
     if(param.seriesName=="营销库"){
-
         input[4].value=param.data.name;
+        if(param.data.children==undefined){
+            bool2=false;
+        }else {
+            bool2=true;
+        }
     }
+
+
 }
+
--- src/js/myjs/index.js
+++ src/js/myjs/index.js
@@ -1,3 +1,64 @@
 
 
+//false 代表三层 true 代表二层
+function processData(input1,input2,input3,input4,bool1,bool2) {
+    console.log(arguments);
+    var PMS=[];
+    var Sale=[];
+    mockData.mapdata.map(function (item) {
+        if(item.source=='PMS'){
+            PMS.push(item);
+        };
+        if(item.source=='营销'){
+            Sale.push(item);
+        }
+    })
 
+    console.log(PMS);
+    console.log(Sale);
+    var obj={PMS:[],Sale:[]};
+    if(bool1){
+        PMS.map(function (item) {
+            if(item.nodes[1].LINE_NAME.trim()==input2){
+                item.nodes.map(function (item) {
+                    obj.PMS.push(item)
+                })
+            }
+        })
+    }else {
+        PMS.map(function (item) {
+            for(var i=2;i<item.nodes.length;i++){
+                if(item.nodes[i].EQUIP_NAME.trim()==input2){
+                    obj.PMS.push(item.nodes[0])
+                    obj.PMS.push(item.nodes[1])
+                    obj.PMS.push(item.nodes[i])
+                    break;
+                }
+            }
+        })
+    }
+
+    if(bool2){
+        Sale.map(function (item) {
+            if(item.nodes[1].LINE_NAME.trim()==input4){
+                item.nodes.map(function (item) {
+                    obj.Sale.push(item)
+                })
+            }
+        })
+    }else {
+        Sale.map(function (item) {
+            for(var i=2;i<item.nodes.length;i++){
+                if(item.nodes[i].EQUIP_NAME.trim()==input4){
+                    obj.Sale.push(item.nodes[0])
+                    obj.Sale.push(item.nodes[1])
+                    obj.Sale.push(item.nodes[i])
+                    break;
+                }
+            }
+        })
+    }
+    console.log(obj);
+
+
+}
